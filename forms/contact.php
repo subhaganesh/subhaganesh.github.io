d0ccc10818987acdf32e26f6cecdafcc5327c82a
subhaganesh <subhaganesh5@outlook.com>
2024-02-18 20:35:42 +0530

    update

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'https://formspree.io/f/mleqbokw';
+  $receiving_email_address = 'subhaganesh5@outlook.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
